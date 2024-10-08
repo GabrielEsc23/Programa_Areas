de147010e1f2cc9f911f96c06e6dda5ef5e8c474
Gabriel Escobar <gabrielescobar283@gmail.com>
2024-10-08 00:16:08 -0500

    Hice las primeras módificaciones

--- src/Programa_Areas.java
+++ src/Programa_Areas.java
@@ -5,9 +5,9 @@ public class Programa_Areas{
         Scanner numero=new Scanner(System.in);
         int opcion;
         System.out.println("Selecciona una opcion" +
-                "1.Área del triángulo" +
-                "2.Área del cuadrado" +
-                "3.Área del círculo: ");
+                "1.Área del Triángulo" +
+                "2.Área del Cuadrado" +
+                "3.Área del Círculo: ");
          opcion=numero.nextInt();
         if (opcion==1){
             Scanner entrada=new Scanner(System.in);
@@ -17,7 +17,7 @@ public class Programa_Areas{
             System.out.println("Introduzca el valor de altura del triangulo");
             altura=entrada.nextFloat();
             resultado=(base*altura)/2;
-            System.out.println("El resultado es:"+resultado);
+            System.out.println("El valor del área del triángulo es de: "+resultado);
 
         }
     }
